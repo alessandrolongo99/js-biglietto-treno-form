14506fb13ec5927551a40060e68355f7f9540af2
alessandrolongo99 <longoalessandro153@gmail.com>
2022-06-15 17:16:39 +0200

    migliorie

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
-let km = 0;
-let age = 0;
-let myName = 0;
-const tax = 0.26;
-
 const submit = document.getElementById('submit');
 const cancel = document.getElementById('cancel');
 
+document.getElementById('ticket-box').style.display = 'none';
+
 submit.addEventListener('click',
     function () {
+        let km = 0;
+        let age = 0;
+        let myName = 0;
+        const tax = 0.26;
+
         myName = document.getElementById('name').value;
         console.log(myName);
 
@@ -18,32 +20,41 @@ submit.addEventListener('click',
         console.log(age);
 
         let price = km * tax;
+        let discount = 'Biglietto standard';
 
         if (age == 1) {
             price -= ((price / 100) * 15);
+            discount = 'Biglietto minorenni';
         } else if (age == 2) {
             price -= ((price / 100) * 35);
+            discount = 'Biglietto over 65';
         }
 
         let cab = Math.floor((Math.random() * 10) + 1);
         let cp = Math.floor(Math.random() * 10000);
 
         document.getElementById('passenger-name').innerHTML = myName;
+        document.getElementById('discount').innerHTML = discount;
         document.getElementById('passenger-cab').innerHTML = cab;
         document.getElementById('passenger-cp').innerHTML = cp;
         document.getElementById('ticket').innerHTML = price.toFixed(2) + '€';
+
+        document.getElementById('ticket-box').style.display = 'block';
     }
 );
 
 cancel.addEventListener('click',
-    function(){
-        myName = document.getElementById('name').value = null;
-        km = document.getElementById('km').value = null;
-        age = document.getElementById('age').value = null;
+    function () {
+        document.getElementById('name').value = null;
+        document.getElementById('km').value = null;
+        document.getElementById('age').value = null;
 
         document.getElementById('passenger-name').innerHTML = null;
+        document.getElementById('discount').innerHTML = null;
         document.getElementById('passenger-cab').innerHTML = null;
         document.getElementById('passenger-cp').innerHTML = null;
         document.getElementById('ticket').innerHTML = null;
+
+        document.getElementById('ticket-box').style.display = 'none';
     }
 );
